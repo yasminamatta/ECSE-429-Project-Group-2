5c19fb5ce092e4741d0371be047c88a67275f14e
himelsaha29 <himelsaha29@live.com>
2023-02-02 17:44:41 -0500

    Spring boot backend set up

---
+++ src/main/java/com/software/validation/ECSE429/Ecse429Application.java
@@ -0,0 +1,13 @@
+package com.software.validation.ECSE429;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecse429Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecse429Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/software/validation/ECSE429/Ecse429ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.validation.ECSE429;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecse429ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
