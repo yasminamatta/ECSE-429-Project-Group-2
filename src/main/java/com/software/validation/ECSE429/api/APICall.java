9b1ce0e5fa8975a67346ffdd00635ef6ffe7b9aa
himelsaha29 <himelsaha29@live.com>
2023-02-02 22:10:21 -0500

    POST method works

--- src/main/java/com/software/validation/ECSE429/api/APICall.java
+++ src/main/java/com/software/validation/ECSE429/api/APICall.java
@@ -1,25 +1,42 @@
 package com.software.validation.ECSE429.api;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
+import org.json.simple.JSONObject;
+
+import java.io.IOException;
 
 public class APICall {
 
 
     public static void main(String[] args) {
         APICall ap = new APICall();
+
+
     }
 
-    public Response post(String url) {
+    public Response post(String url, String contentType, JSONObject jsonBody) {
         OkHttpClient client = new OkHttpClient();
         Response response = null;
+
+        MediaType JSON = MediaType.parse("application/" + contentType + "; charset=utf-8");
+        RequestBody body = RequestBody.create(JSON, jsonBody.toString());
+
         Request request = new Request.Builder()
                 .url(url)
+                .addHeader("content-type", "application/" + contentType + "; charset=utf-8")
+                .post(body)
                 .build();
+
+        try {
+            response = client.newCall(request).execute();
+        } catch (IOException e) {
+            return null;
+        }
 
 
         return response;
     }
+
+
 
 }
--- src/test/java/com/software/validation/ECSE429/TodosTest.java
+++ src/test/java/com/software/validation/ECSE429/TodosTest.java
@@ -1,21 +1,46 @@
 package com.software.validation.ECSE429;
 
+import com.software.validation.ECSE429.api.APICall;
+import okhttp3.Response;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.junit.Test;
+
+import java.io.IOException;
 
 public class TodosTest {
 
-
-
-    public void test() {
-        try {
-            JSONParser parser = new JSONParser();
-            JSONObject json = (JSONObject) parser.parse("");
-            // System.out.println(json.get(""));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
+        TodosTest td = new TodosTest();
+        td.test();
     }
 
+    @Test
+    public void test() {
+
+        APICall ap = new APICall();
+        JSONObject js = new JSONObject();
+        js.put("title", "mcgill");
+        js.put("description", "okhttp");
+        Response response = ap.post("http://localhost:4567/todos", "json", js);
+
+        String res = null;
+
+        try {
+            res = response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            System.out.println(res);
+            JSONParser parser = new JSONParser();
+            JSONObject json = (JSONObject) parser.parse(res);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.out.println("Error");
+        }
+    }
 }
