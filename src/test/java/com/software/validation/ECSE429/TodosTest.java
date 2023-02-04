e3d23fe1b62dfd3640390fc104d146e1a081b209
himelsaha29 <himelsaha29@live.com>
2023-02-03 23:09:33 -0500

    Ensure only 1 item created

--- src/test/java/com/software/validation/ECSE429/TodosTest.java
+++ src/test/java/com/software/validation/ECSE429/TodosTest.java
@@ -17,6 +17,7 @@ import java.util.List;
 public class TodosTest {
 
     Integer successCodes[] = {200, 201};
+    int todos[] = {0, 0};
 
     public static void main(String[] args) {
 
@@ -27,6 +28,28 @@ public class TodosTest {
     @Test
     public void test() {
         APICall ap = new APICall();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Response size = ap.get("todos", "json");
+                JSONParser parser = new JSONParser();
+                JSONObject json = null;
+                try {
+                    json = (JSONObject) parser.parse(size.body().string());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                todos[0] = ((JSONArray)(json.get("todos"))).size();
+            }
+        });
+
+        t1.start();
+        try {
+            t1.join();
+        } catch (Exception e) {
+
+        }
+
 
         JSONObject js = new JSONObject();
         js.put("title", "mcgill");
@@ -47,13 +70,39 @@ public class TodosTest {
             System.out.println("title == mcgill - TEST PASSED");
             Assert.assertEquals("okhttp", json.get("description"));
             System.out.println("description == okhttp - TEST PASSED");
-            Assert.assertTrue(Arrays.asList(successCodes).contains(response.code()));
-            System.out.println("status code == 200 - TEST PASSED");
+            int code = response.code();
+            Assert.assertTrue(Arrays.asList(successCodes).contains(code));
+            System.out.println("status code == " + code + " - TEST PASSED");
 
         } catch (ParseException e) {
             e.printStackTrace();
             System.out.println("Error");
         }
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Response size2 = ap.get("todos", "json");
+                JSONParser parser = new JSONParser();
+                JSONObject json = null;
+                try {
+                    json = (JSONObject) parser.parse(size2.body().string());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                todos[1] = ((JSONArray)(json.get("todos"))).size();
+            }
+        });
+
+        t2.start();
+        try {
+            t2.join();
+        } catch (Exception e) {
+
+        }
+
+        Assert.assertEquals(1, Math.abs(todos[1] - todos[0]));
+        System.out.println("only " + Math.abs(todos[1] - todos[0]) + " todo created - TEST PASSED");
 
     }
 }
