29ad671b2ea7f6a9982d452db6ff45d6f35cf0c3
himelsaha29 <himelsaha29@live.com>
2023-02-06 17:11:52 -0500

    HEAD method created

--- src/main/java/com/software/validation/ECSE429/api/APICall.java
+++ src/main/java/com/software/validation/ECSE429/api/APICall.java
@@ -10,7 +10,12 @@ public class APICall {
 
     public static void main(String[] args) {
         APICall ap = new APICall();
-
+        Response x = ap.head("todos", "json");
+        try {
+            System.out.println(x.headers().toString());
+        } catch (Exception e ){
+            e.printStackTrace();
+        }
 
     }
 
@@ -38,6 +43,31 @@ public class APICall {
         return response;
     }
 
+
+    public Response put(String url, String contentType, JSONObject jsonBody) {
+        OkHttpClient client = new OkHttpClient();
+        Response response = null;
+        url = "http://localhost:4567/" + url;
+
+        MediaType JSON = MediaType.parse("application/" + contentType + "; charset=utf-8");
+        RequestBody body = RequestBody.create(JSON, jsonBody.toString());
+
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("content-type", "application/" + contentType + "; charset=utf-8")
+                .put(body)
+                .build();
+
+        try {
+            response = client.newCall(request).execute();
+        } catch (IOException e) {
+            return null;
+        }
+
+
+        return response;
+    }
+
     public Response get(String url, String contentType) {
         OkHttpClient client = new OkHttpClient();
         Response response = null;
@@ -46,6 +76,27 @@ public class APICall {
         Request request = new Request.Builder()
                 .url(url)
                 .addHeader("content-type", "application/" + contentType + "; charset=utf-8")
+                .build();
+
+        try {
+            response = client.newCall(request).execute();
+        } catch (IOException e) {
+            return null;
+        }
+
+
+        return response;
+    }
+
+    public Response head(String url, String contentType) {
+        OkHttpClient client = new OkHttpClient();
+        Response response = null;
+        url = "http://localhost:4567/" + url;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("content-type", "application/" + contentType + "; charset=utf-8")
+                .head()
                 .build();
 
         try {
