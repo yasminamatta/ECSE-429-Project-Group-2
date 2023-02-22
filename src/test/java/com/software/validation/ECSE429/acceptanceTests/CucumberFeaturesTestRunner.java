c2e188318f60251c0e98cba67b78b904798a87c2
himelsaha29 <himelsaha29@live.com>
2023-02-22 16:24:29 -0500

    Cucumber initialized

---
+++ src/test/java/com/software/validation/ECSE429/acceptanceTests/CucumberFeaturesTestRunner.java
@@ -0,0 +1,11 @@
+package com.software.validation.ECSE429.acceptanceTests;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "pretty", features = "src/test/java/com/software/validation/ECSE429/acceptanceTests")
+public class CucumberFeaturesTestRunner {
+}
+
---
+++ src/test/java/com/software/validation/ECSE429/acceptanceTests/todos/StepDefinitions.java
@@ -0,0 +1,20 @@
+package com.software.validation.ECSE429.acceptanceTests.todos;
+
+import io.cucumber.java.en.Given;
+import org.junit.Assert;
+
+public class StepDefinitions {
+
+    @Given("the server is running")
+    public void the_server_is_running() {
+        Runtime rt = Runtime.getRuntime();
+        try {
+            Process pr = rt.exec("java -jar runTodoManagerRestAPI-1.5.5.jar"); // Ensures that the API is ready to be tested
+            //System.out.println("Setting up environment");
+            Thread.sleep(4000);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.assertEquals("Server", "error");
+        }
+    }
+}
